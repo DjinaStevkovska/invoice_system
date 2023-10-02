8f0ac3e0bb4699c65536a280ccea715b7a6c5adc
DjinaStevkovska <stevkovskadjina@yahoo.com>
2023-10-02 18:00:53 +0200

    example templating

---
+++ src/Controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", name="test")
+     */
+    public function index(): Response
+    {
+        $data = [
+            'user_name' => 'Djina Stevkovska',
+            'welcome_message' => 'Welcome and have a great journey creating invoices!',
+            'current_year' => date('Y'),
+            'site_name' => 'Invoice System',
+        ];
+        return $this->render('pages/child.html.twig', $data);
+    }
+}
